53437c66a58fccd05424189e660feb4faf0e4f72
LGD12345 <lkd1126@naver.com>
2023-11-18 23:23:13 +0900

    start.1

---
+++ ch1/start_app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello';
+
+// shift + alt + 아래
+
+function App() {
+  return (
+<div>
+  <Hello /> 
+  <Hello />
+  
+</div>
+  );
+}
+
+export default App;
